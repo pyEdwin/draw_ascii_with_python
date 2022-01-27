b3f5863fcf59942947225f4e6c8de0d2fe44e2a2
Yapnounkie <edmundopierre@yahoo.com>
2022-01-26 17:49:20 -0800

    second loop reversed

--- draw_with_python.py
+++ draw_with_python.py
@@ -5,4 +5,14 @@ for i in range (length):
     for j in range(length-i):
         print(" ", end ='')
     print(st)
-    st +='*'    
+    st +='*'  
+
+length -=1
+ast_num = (length*2)-1
+st = "*"   * ast_num
+space = 1
+for i in range(length):
+    print(" " + space * " " + st)
+    ast_num -=2
+    st = "*"  * ast_num
+    space += 1
